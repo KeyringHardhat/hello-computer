e594bee59d180e5f31ba76c791860fc15256a794
Kieron Harding <kieron.harding1996@gmail.com>
2025-03-09 13:02:13 +0000

    ubuntu fixes, readme update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hello-computer
 A collection of scripts to kickstart setup of a new machine. 
+
+* `mac-setup.sh` - Configure new Mac. 
+* `ubuntu-setup.sh` - Configure new Ubuntu install, usually under WSL. 
--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -5,6 +5,8 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+USER_HOME=$(eval echo ~$SUDO_USER)
+
 echo "Updating and upgrading packages..."
 apt update && apt upgrade -y
 
@@ -24,31 +26,32 @@ apt install -y \
 
 echo "Installing Docker..."
 apt install -y docker.io
-usermod -aG docker $USER
-newgrp docker
+usermod -aG docker $SUDO_USER
+echo "Docker installed. You may need to log out and back in for group changes to apply."
 
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
+if [ ! -d "$USER_HOME/.oh-my-zsh" ]; then
     echo "Installing Oh My Zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-    chsh -s $(which zsh)
+    sudo -u $SUDO_USER sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    chsh -s $(which zsh) $SUDO_USER
 else
     echo "Oh My Zsh already installed."
 fi
 
-if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+if [ ! -d "$USER_HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
     echo "Installing Zsh Autosuggestions..."
-    git clone https://github.com/zsh-users/zsh-autosuggestions \
-        ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    sudo -u $SUDO_USER git clone https://github.com/zsh-users/zsh-autosuggestions \
+        "$USER_HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 fi
 
 echo "Setting up Passion theme..."
-wget -O $HOME/.oh-my-zsh/themes/passion.zsh-theme https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme
+sudo -u $SUDO_USER wget -O "$USER_HOME/.oh-my-zsh/themes/passion.zsh-theme" \
+    https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme
 
 echo "Configuring Zsh..."
-cat <<EOF > $HOME/.zshrc
-export ZSH="$HOME/.oh-my-zsh"
+sudo -u $SUDO_USER tee "$USER_HOME/.zshrc" > /dev/null <<EOF
+export ZSH="$USER_HOME/.oh-my-zsh"
 ZSH_THEME="passion"
-plugins=(git zsh-autosuggestions)
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 source \$ZSH/oh-my-zsh.sh
 EOF
 
@@ -56,15 +59,15 @@ echo "Setting up Git..."
 read -p "Enter your Git name: " git_name
 read -p "Enter your Git email: " git_email
 
-git config --global user.name "$git_name"
-git config --global user.email "$git_email"
-git config --global core.editor "vim"
+sudo -u $SUDO_USER git config --global user.name "$git_name"
+sudo -u $SUDO_USER git config --global user.email "$git_email"
+sudo -u $SUDO_USER git config --global core.editor "vim"
 
-if [ ! -f "$HOME/.ssh/id_rsa" ]; then
+if [ ! -f "$USER_HOME/.ssh/id_rsa" ]; then
     echo "Generating SSH key..."
-    ssh-keygen -t rsa -b 4096 -C "$git_email" -f "$HOME/.ssh/id_rsa" -N ""
-    echo "Your public key:"
-    cat "$HOME/.ssh/id_rsa.pub"
+    sudo -u $SUDO_USER ssh-keygen -t rsa -b 4096 -C "$git_email" -f "$USER_HOME/.ssh/id_rsa" -N ""
+    echo "Your public key (add this to GitHub/GitLab):"
+    cat "$USER_HOME/.ssh/id_rsa.pub"
 fi
 
 echo "Setup complete! Restart your terminal and enjoy your new setup. 🚀"
